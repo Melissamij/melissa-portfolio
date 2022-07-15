7817e7a3fd871529cf09d24f0606a3fdea168dd2
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-07-15 12:01:01 +0200

    js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,3 @@
-$(document).ready(function (){
-    var maxHeight = 0;
-    for(i=0;i<$(".card").length;i++){
-      if($(".card").eq(i)){
-        var currentHeight = $(".card").eq(i).height();
-        if(currentHeight>=maxHeight){
-          maxHeight = currentHeight;
-        }
-      }
-      else{
-        break;
-      }
-    }
-    $(".card").height(maxHeight);
-  });
-
 
   $('.hamburger-button').click(function(){
     $('.mobile-menu').fadeToggle(100);
