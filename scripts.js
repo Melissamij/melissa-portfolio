ab378f422e641d1af45bc5fa0e5951a19a33056c
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-08-10 11:04:39 +0200

    test collaps

--- scripts.js
+++ scripts.js
@@ -15,3 +15,37 @@ lightGallery(document.getElementById('lightgallery'), {
     download: false
     
 });
+
+
+// collapsible section
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+// animated collapsible slide down
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
