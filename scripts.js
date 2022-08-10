5bf6475724d1a116729dca959808bbb27f32486e
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-08-10 11:37:56 +0200

    test collaps

--- scripts.js
+++ scripts.js
@@ -17,22 +17,7 @@ lightGallery(document.getElementById('lightgallery'), {
 });
 
 
-// collapsible section
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
+// collaps
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -44,6 +29,6 @@ for (i = 0; i < coll.length; i++) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
-    }
+    } 
   });
 }
