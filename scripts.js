0686a6eb7c8c67c2a52ade42a6e7ddb2ab6677e5
Melissamij <102660511+Melissamij@users.noreply.github.com>
2022-08-16 12:53:44 +0200

    arrow try

--- scripts.js
+++ scripts.js
@@ -10,5 +10,49 @@ AOS.init({
 });
 
 
+// code to show / hide button to top 
 
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
 
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+// Go to top 
+const goToTop = () => {
+  document.body.scrollIntoView();
+};
+
+backToTopButton.addEventListener("click", goToTop)
+
+// making scroll behavior smooth
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+// progress bar
+const pageProgressBar = document.querySelector(".progress-bar")
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+  
+  pageProgressBar.style.width = `${scrolledPercentage}%`
+  
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
